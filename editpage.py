0d535e2dc3406ff243a41045c79e95e5b669227c
ecki <ecki@ecki-netz.de>
2024-12-27 23:06:48 +0100

    update includes

--- editpage.py
+++ editpage.py
@@ -1,20 +1,20 @@
+import logging
+import notehelper
 import os
-import sys
 import shutil
+import sys
 
 import PySignal
-
-from PyQt6 import QtWidgets, QtCore, QtGui
-from PyQt6.QtCore import pyqtSlot
-import logging
-
-from notehelper import text_2_html
+import PyQt6
+import PyQt6.QtCore
+import PyQt6.QtGui
+import PyQt6.QtWidgets
 
 
 logger = logging.getLogger(__name__)
 
 
-class EditPage(QtWidgets.QWidget):
+class EditPage(PyQt6.QtWidgets.QWidget):
     ascii_file_changed = PySignal.Signal()
     project_data_changed = PySignal.Signal()
     project_new_file = PySignal.Signal()
@@ -34,16 +34,16 @@ class EditPage(QtWidgets.QWidget):
         logger.info("edit {} from project {}".format(file_name, project_name))
 
         # ui
-        main_layout = QtWidgets.QVBoxLayout()
-        self.text_field = QtWidgets.QPlainTextEdit()
+        main_layout = PyQt6.QtWidgets.QVBoxLayout()
+        self.text_field = PyQt6.QtWidgets.QPlainTextEdit()
         main_layout.addWidget(self.text_field)
         main_layout.addWidget(self.init_format_field())
 
         # settings
         self.setLayout(main_layout)
         #self.resize(900, 500)
-        self.text_field.setLineWrapMode(QtWidgets.QPlainTextEdit.LineWrapMode.NoWrap)
-        self.setWindowModality(QtCore.Qt.WindowModality.ApplicationModal)
+        self.text_field.setLineWrapMode(PyQt6.QtWidgets.QPlainTextEdit.LineWrapMode.NoWrap)
+        self.setWindowModality(PyQt6.QtCore.Qt.WindowModality.ApplicationModal)
 
         # signals
         self.connect_text_field_signals()
@@ -61,39 +61,39 @@ class EditPage(QtWidgets.QWidget):
 
     def init_format_field(self):
         # main widget and layout
-        format_container = QtWidgets.QWidget(self)
-        format_layout = QtWidgets.QGridLayout()
+        format_container = PyQt6.QtWidgets.QWidget(self)
+        format_layout = PyQt6.QtWidgets.QGridLayout()
         format_container.setLayout(format_layout)
 
         # internal link button
-        show_docs_btn = QtWidgets.QPushButton("show docs")
+        show_docs_btn = PyQt6.QtWidgets.QPushButton("show docs")
         show_docs_btn.setToolTip("<b>show all linkable internal documents</b>")
         format_layout.addWidget(show_docs_btn, 0, 0)
         show_docs_btn.clicked.connect(self.on_show_docs)
 
         # open git
-        open_git_btn = QtWidgets.QPushButton("Commits")
+        open_git_btn = PyQt6.QtWidgets.QPushButton("Commits")
         format_layout.addWidget(open_git_btn, 0, 1)
         open_git_btn.clicked.connect(self.on_open_git)
 
         # save btn
-        save_btn = QtWidgets.QPushButton("Speichern")
+        save_btn = PyQt6.QtWidgets.QPushButton("Speichern")
         format_layout.addWidget(save_btn, 1, 0)
         save_btn.clicked.connect(self.on_save_changes)
 
         # reset changes button
-        discard_btn = QtWidgets.QPushButton("Verwerfen")
+        discard_btn = PyQt6.QtWidgets.QPushButton("Verwerfen")
         format_layout.addWidget(discard_btn, 1, 1)
         discard_btn.clicked.connect(self.on_discard_changes)
 
         # upload
-        upload_file_btn = QtWidgets.QPushButton("Upload File")
+        upload_file_btn = PyQt6.QtWidgets.QPushButton("Upload File")
         upload_file_btn.setToolTip("<b>Add files (img, pdf, ..) to project</b>")
         format_layout.addWidget(upload_file_btn, 0, 3)
         upload_file_btn.clicked.connect(self.on_upload)
 
         # info button
-        info_button = QtWidgets.QPushButton("ℹ️")
+        info_button = PyQt6.QtWidgets.QPushButton("ℹ️")
         format_layout.addWidget(info_button, 1, 3)
         info_button.clicked.connect(self.on_click_info)
 
@@ -102,11 +102,11 @@ class EditPage(QtWidgets.QWidget):
     def on_discard_changes(self):
         if not self.changed:
             return
-        reply = QtWidgets.QMessageBox.question(self, "Text verwerfen",
+        reply = PyQt6.QtWidgets.QMessageBox.question(self, "Text verwerfen",
                             "Wollen Sie den Text wirklich verwerfen?",
-                            QtWidgets.QMessageBox.StandardButton.Yes | QtWidgets.QMessageBox.StandardButton.No,
-                            QtWidgets.QMessageBox.StandardButton.No)
-        if reply == QtWidgets.QMessageBox.StandardButton.Yes:
+                            PyQt6.QtWidgets.QMessageBox.StandardButton.Yes | PyQt6.QtWidgets.QMessageBox.StandardButton.No,
+                            PyQt6.QtWidgets.QMessageBox.StandardButton.No)
+        if reply == PyQt6.QtWidgets.QMessageBox.StandardButton.Yes:
             logger.info("discarding changes")
             self.text_field.clear()
             self.load_content()
@@ -122,8 +122,6 @@ class EditPage(QtWidgets.QWidget):
     def on_open_git(self):
         logger.info("open git clicked")
         pass
-        # self.git_window = GitWindow(git_repo=self.repo, project_path=self.project_path, html_handler=self.html_handler, debug=self.debug)
-        # self.git_window.show()
 
     def on_upload(self):
         logger.info("upload clicked")
@@ -133,7 +131,7 @@ class EditPage(QtWidgets.QWidget):
         else:
             import_dir = os.path.join(import_dir, "Downloads")
         import_dir = self.project_data.get("import_dir", import_dir)
-        import_file = QtWidgets.QFileDialog.getOpenFileName(self, "Import file", import_dir, "*")
+        import_file = PyQt6.QtWidgets.QFileDialog.getOpenFileName(self, "Import file", import_dir, "*")
         if not import_file:
             logger.warning("no file for upload selected")
             return
@@ -150,7 +148,7 @@ class EditPage(QtWidgets.QWidget):
             logger.error("no project path set")
             return
         file_name = os.path.join(copy_dir, file_name)
-        copy_file = QtWidgets.QFileDialog.getSaveFileName(self, "Save file to project path", file_name, "*")
+        copy_file = PyQt6.QtWidgets.QFileDialog.getSaveFileName(self, "Save file to project path", file_name, "*")
         if not copy_file:
             logger.warning("no file for upload target selected")
             return
@@ -173,24 +171,16 @@ class EditPage(QtWidgets.QWidget):
             shutil.copy(import_file, copy_file)
             self.project_new_file.emit(file_name)
 
-    def on_enter_pressed(self, block_nr):
-        prev_line_text = self.text_field.textCursor().block().previous().text()
-
-        if not prev_line_text:
-            return
-
-        self.text_field.setFocus()
-
     def on_click_info(self):
         logger.info("info clicked")
-        url = QtCore.QUrl("https://docs.asciidoctor.org/asciidoc/latest/syntax-quick-reference/")
-        reply = QtWidgets.QMessageBox.question(self, "Syntax Information",
+        url = PyQt6.QtCore.QUrl("https://docs.asciidoctor.org/asciidoc/latest/syntax-quick-reference/")
+        reply = PyQt6.QtWidgets.QMessageBox.question(self, "Syntax Information",
                         "Open Asciidoc Quick Reference {}".format(url),
-                        QtWidgets.QMessageBox.StandardButton.Yes | QtWidgets.QMessageBox.StandardButton.No,
-                        QtWidgets.QMessageBox.StandardButton.No)
-        if reply == QtWidgets.QMessageBox.StandardButton.Yes:
+                        PyQt6.QtWidgets.QMessageBox.StandardButton.Yes | PyQt6.QtWidgets.QMessageBox.StandardButton.No,
+                        PyQt6.QtWidgets.QMessageBox.StandardButton.No)
+        if reply == PyQt6.QtWidgets.QMessageBox.StandardButton.Yes:
             logger.debug("open link in external browser")
-            QtGui.QDesktopServices.openUrl(url)
+            PyQt6.QtGui.QDesktopServices.openUrl(url)
         return
 
     def on_text_changed(self):
@@ -204,11 +194,12 @@ class EditPage(QtWidgets.QWidget):
         logger.debug("save changes in {}".format(text_file_path))
         text = self.text_field.toPlainText()
         try:
-            html_text = text_2_html(text)
+            html_text = notehelper.text_2_html(text)
         except Exception as e:
             logger.error("error in asciidoc: {}".format(e))
             self.on_discard_changes()
             return
+        logger.debug("new html_text would be: {}".format(html_text))
         with open(text_file_path, "w") as text_file:
             text_file.writelines(text)
         self.changed = False
@@ -222,7 +213,6 @@ class EditPage(QtWidgets.QWidget):
         self.disconnect_text_field_signals()
 
         self.text_field.clear()
-        text = ""
         with open(text_file_path, "r") as text_file:
             text = text_file.readlines()
         for line in text:
@@ -237,24 +227,24 @@ class EditPage(QtWidgets.QWidget):
     def disconnect_text_field_signals(self):
         self.text_field.textChanged.disconnect(self.on_text_changed)
 
-    @pyqtSlot(QtGui.QCloseEvent)
+    @PyQt6.QtCore.pyqtSlot(PyQt6.QtGui.QCloseEvent)
     def closeEvent(self, event):
         logger.info("Trying to close window")
         geometry = (self.frameGeometry().x(), self.frameGeometry().y(), self.frameGeometry().width(), self.frameGeometry().height())
         self.geometry_update.emit(geometry)
         if self.changed:
-            msg_box = QtWidgets.QMessageBox()
+            msg_box = PyQt6.QtWidgets.QMessageBox()
             msg_box.setText("Möchten Sie die Änderungen Speichern?")
 
-            yes_btn = msg_box.addButton(QtWidgets.QMessageBox.StandardButton.Yes)
+            yes_btn = msg_box.addButton(PyQt6.QtWidgets.QMessageBox.StandardButton.Yes)
             yes_btn.setText("Speichern")
-            no_btn = msg_box.addButton(QtWidgets.QMessageBox.StandardButton.No)
+            no_btn = msg_box.addButton(PyQt6.QtWidgets.QMessageBox.StandardButton.No)
             no_btn.setText("Nicht speichern")
 
             msg_box.setDefaultButton(yes_btn)
 
             ret = msg_box.exec()
-            if ret == QtWidgets.QMessageBox.StandardButton.Yes:
+            if ret == PyQt6.QtWidgets.QMessageBox.StandardButton.Yes:
                 logger.info("close window - save changes")
                 self.on_save_changes()
             else:
@@ -269,8 +259,8 @@ if __name__ == "__main__":
 
     logger.info("moin")
 
-    app = QtWidgets.QApplication(sys.argv)
-    ex = EditPage({"path": "/home/ecki/tmp2/notebooks/test1", "create_date": 1734897219.1147738,
+    app = PyQt6.QtWidgets.QApplication(sys.argv)
+    ex = EditPage({"path": "/home/ecki/temp/notebooks/test1", "create_date": 1734897219.1147738,
             "last_ascii_file": "index.asciidoc"}, "test1", "index.asciidoc")
     ex.show()
     sys.exit(app.exec())
